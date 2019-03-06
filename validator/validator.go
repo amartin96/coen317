d76b203b6b40259b1cdd16315c408426bdf8551a
Alex Martin <atmartin@scu.edu>
2019-03-05 17:17:25 -0800

    Create validator.go

---
+++ validator/validator.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"coen317/common"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+)
+
+func getMap(file io.Reader) map[int32]int {
+	retval := make(map[int32]int)
+	for {
+		var x int32
+		if err := binary.Read(file, binary.BigEndian, x); err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		retval[x]++
+	}
+	return retval
+}
+
+func Validate(file1 io.Reader, file2 io.Reader) bool {
+	return reflect.DeepEqual(getMap(file1), getMap(file2))
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %v file1 file2\n", os.Args[0])
+		return
+	}
+
+	file1, err := os.Open(os.Args[1])
+	common.PanicOnError(err)
+	defer common.Close(file1)
+	file2, err := os.Open(os.Args[2])
+	common.PanicOnError(err)
+	defer common.Close(file2)
+	fmt.Printf("%v\n", Validate(file1, file2))
+}
